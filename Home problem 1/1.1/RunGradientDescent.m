f81bede93bd9af25cc48e30ed03221f715ef772c
Mattias <klutten99@gmail.com>
2021-09-16 14:35:33 +0200

    1.1 Checked for hand in

--- Home problem 1/1.1/RunGradientDescent.m
+++ Home problem 1/1.1/RunGradientDescent.m
@@ -3,11 +3,9 @@
 
 function x = RunGradientDescent(xStart, mu, eta, gradientTolerance)
     x = xStart;
-    while true
+    gradient = ComputeGradient(x, mu);
+    while norm(gradient) > gradientTolerance
         gradient = ComputeGradient(x, mu);
         x = x - eta*gradient;
-        if norm(gradient) < gradientTolerance
-            return
-        end
     end
 end
