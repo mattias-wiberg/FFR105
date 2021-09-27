be1611156735487a4764d04bcb4276a313e54803
Mattias <klutten99@gmail.com>
2021-09-27 13:29:42 +0200

    New ignore

--- Home problem 1/1.3/RunSingle.m
+++ Home problem 1/1.3/RunSingle.m
@@ -13,11 +13,10 @@ crossoverProbability = 1;        % Changes allowed (= pCross)
 mutationProbability = 0.1;        % Changes allowed. (Note: 0.02 <=> 1/numberOfGenes)
 numberOfGenerations = 1000;        % Changes allowed.
 
-for i = 1:10
-    [maximumFitness(i), bestVariableValues(:,i)] = RunFunctionOptimization(populationSize, numberOfGenes, numberOfVariables, maximumVariableValue, tournamentSize, ...
-                                       tournamentProbability, crossoverProbability, mutationProbability, numberOfGenerations);
-                                   
-    sprintf('Fitness: %d, x(1): %0.10f, x(2): %0.10f', maximumFitness(i), bestVariableValues(1,i), bestVariableValues(2,i))
-end
+[maximumFitness(i), bestVariableValues(:,i)] = RunFunctionOptimization(populationSize, numberOfGenes, numberOfVariables, maximumVariableValue, tournamentSize, ...
+                                   tournamentProbability, crossoverProbability, mutationProbability, numberOfGenerations);
+
+sprintf('Fitness: %d, x(1): %0.10f, x(2): %0.10f', maximumFitness(i), bestVariableValues(1,i), bestVariableValues(2,i))
+
 
 
