88a7e15b9b7f11fdd80ecec48e8e3d3aed802765
Mattias <klutten99@gmail.com>
2021-10-04 16:08:40 +0200

    Skeleton renamed

---
+++ Home problem 2/1. The traveling salesman/GeneratePath.m
@@ -0,0 +1,6 @@
+function path = GeneratePath(pheromoneLevel, visibility, alpha, beta)
+%GENERATEPATH Summary of this function goes here
+%   Detailed explanation goes here
+    
+end
+
---
+++ Home problem 2/1. The traveling salesman/GetNode.m
@@ -0,0 +1,6 @@
+function nextNode = GetNode(tabuList, pheromoneLevel, visibility, alpha, beta)
+%GETNODE Summary of this function goes here
+%   Detailed explanation goes here
+    
+end
+
