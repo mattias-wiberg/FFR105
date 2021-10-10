20c2d220ab5668cb2df175fe16a28bfff02c0a8c
Mattias <klutten99@gmail.com>
2021-10-10 16:41:25 +0200

    Update Cross.m

--- Home problem 2/4. Function fitting using LGP/Cross.m
+++ Home problem 2/4. Function fitting using LGP/Cross.m
@@ -1,8 +1,9 @@
-function newIndividuals = Cross(individual1, individual2)
+function newIndividuals = Cross(induvidual1, induvidual2)
     % Two point crossover
-    
+    chromosome1 = induvidual1.Chromosome;
+    chromosome2 = induvidual2.Chromosome;
     instructionSize = 4;
-    numberOfInstructions1 = length(individual1)/instructionSize;
+    numberOfInstructions1 = length(chromosome1)/instructionSize;
     points1 = randi([0 numberOfInstructions1], 2, 1)+1;
     while length(unique(points1)) < 2 
         % the two random points has to be unique else one newIndividual
@@ -11,15 +12,15 @@ function newIndividuals = Cross(individual1, individual2)
     end
     points1 = sort(points1);
     
-    numberOfInstructions2 = length(individual2)/instructionSize;
+    numberOfInstructions2 = length(chromosome2)/instructionSize;
     points2 = randi([0 numberOfInstructions2], 2, 1)+1;
     points2 = sort(points2);
     
-    newIndividuals(1,:) = [individual1(1:points1(1))...
-                           individual2(points2(1)+1:points2(2))...
-                           individual1(points1(2)+1:end)];
-    newIndividuals(2,:) = [individual2(1:points2(1))...
-                           individual1(points1(1)+1:points1(2))...
-                           individual2(points2(2)+1:end)];
+    newIndividuals(1,:) = [chromosome1(1:points1(1))...
+                           chromosome2(points2(1)+1:points2(2))...
+                           chromosome1(points1(2)+1:end)];
+    newIndividuals(2,:) = [chromosome2(1:points2(1))...
+                           chromosome1(points1(1)+1:points1(2))...
+                           chromosome2(points2(2)+1:end)];
     
 end
