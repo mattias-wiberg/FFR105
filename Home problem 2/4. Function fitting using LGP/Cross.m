ac425776791601681562983fb8c07512c25d9395
Mattias <klutten99@gmail.com>
2021-10-10 16:38:09 +0200

    Cross done not tested.

--- Home problem 2/4. Function fitting using LGP/Cross.m
+++ Home problem 2/4. Function fitting using LGP/Cross.m
@@ -1,17 +1,25 @@
 function newIndividuals = Cross(individual1, individual2)
-
-    nGenes = size(individual1,2); % Both chromosomes must have
-                                  % the same length!
-    crossoverPoint = 1 + fix(rand*(nGenes-1));
-
-    newIndividuals = zeros(2,nGenes);
-    for j = 1:nGenes
-        if (j <= crossoverPoint)
-            newIndividuals(1,j) = individual1(j);
-            newIndividuals(2,j) = individual2(j);
-        else
-            newIndividuals(1,j) = individual2(j);
-            newIndividuals(2,j) = individual1(j);
-        end
+    % Two point crossover
+    
+    instructionSize = 4;
+    numberOfInstructions1 = length(individual1)/instructionSize;
+    points1 = randi([0 numberOfInstructions1], 2, 1)+1;
+    while length(unique(points1)) < 2 
+        % the two random points has to be unique else one newIndividual
+        % could have a empty chromosome. Only has to be done for one
+        points1 = randi([0 numberOfInstructions1], 2, 1)+1;
     end
+    points1 = sort(points1);
+    
+    numberOfInstructions2 = length(individual2)/instructionSize;
+    points2 = randi([0 numberOfInstructions2], 2, 1)+1;
+    points2 = sort(points2);
+    
+    newIndividuals(1,:) = [individual1(1:points1(1))...
+                           individual2(points2(1)+1:points2(2))...
+                           individual1(points1(2)+1:end)];
+    newIndividuals(2,:) = [individual2(1:points2(1))...
+                           individual1(points1(1)+1:points1(2))...
+                           individual2(points2(2)+1:end)];
+    
 end
--- Home problem 2/4. Function fitting using LGP/RunLGP.m
+++ Home problem 2/4. Function fitting using LGP/RunLGP.m
@@ -2,13 +2,14 @@ clear;
 clc;
 
 constants = [-1 1 3];
+numberOfConstants = length(constants);
+numberOfOperators = 4; % + - * /
 
-minNumberOfInstructions = 3;
-maxNumberOfInstructions = 10;
+
 populationSize = 10;
 numberOfVariables = 8;
-numberOfConstants = length(constants);
-numberOfOperators = 4;
+minNumberOfInstructions = 3;
+maxNumberOfInstructions = 10;
 
 population = InitializePopulation(populationSize,minNumberOfInstructions,maxNumberOfInstructions,numberOfVariables,numberOfConstants,numberOfOperators);
 
